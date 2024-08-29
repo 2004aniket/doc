6027e3b78bf4415c674807b90c1b7df80c4b19f8
Aniket_Pise <118109413+2004aniket@users.noreply.github.com>
2024-08-29 12:48:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+# import tokenizers
+import streamlit as st
+
+st.title("chatbot")
+
+
+prompt=st.text_input("enter the query")
+if prompt:
+    # model=pipeline(model='Aniket2004/Llama_doctor_v1',task="text-generation")
+    # result=model(f"[INST] {prompt} [/INST]")
+    # st.write(result[0]['generated_text'])
+    st.write(prompt)
