f40821f9478d5671358cf7613c9e61095f0f7735
Aniket_Pise <118109413+2004aniket@users.noreply.github.com>
2024-09-07 10:36:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from transformers import pipeline
 import streamlit as st
 
 st.title("chatbot")
+model=pipeline(model='Aniket2004/Llama_doctor_v1',task="text-generation")
 
 
 prompt=st.text_input("enter the query")
 if prompt:
-    model=pipeline(model='Aniket2004/Llama_doctor_v1',task="text-generation")
     result=model(f"[INST] {prompt} [/INST]")
     st.write(result[0]['generated_text'])
     st.write(prompt)
